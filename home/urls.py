a9767141f62d2d0c8b2cb394577baca0581b2807
Virgo Alpha <bensonkingori96@gmail.com>
2022-07-06 13:24:56 +0300

    map manenoz

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('calculator/', views.calculator, name='calculator'),
+    path('', views.calculator, name='calculator'),
+    # path('calculator/', views.calculator, name='calculator'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 
 def calculator(request):
     return render(request, 'calculator.html')
